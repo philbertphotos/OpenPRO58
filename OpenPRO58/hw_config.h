63d0a40059654939b7ac8e19af53466bb1e0ef4c
MartinLaza <martin.laza@kudrnac.cz>
2017-08-05 19:06:52 +0200

    First commit

---
+++ OpenPRO58/hw_config.h
@@ -0,0 +1,29 @@
+// Buttons on module (required)
+#define PIN_BUTTON_UP           PA8
+#define PIN_BUTTON_MODE         PB14
+#define PIN_BUTTON_DOWN         PB12
+
+// Inputs on module (optional inputs from headset)
+#define PIN_BUTTON_B0           PB13
+#define PIN_BUTTON_B1           PA11
+#define PIN_BUTTON_B2           PB15
+
+#define PIN_BUZZER              PB8
+
+// Receiver module A
+#define PIN_RSSI_A              PA0
+#define PIN_LED_A               PB9
+
+// Receiver module B
+#ifdef USE_DIVERSITY
+    #define PIN_LED_B           PA2
+    #define PIN_RSSI_B          PA1
+#endif
+
+// Receiver switch
+#define PIN_SW                  PC15
+
+//#define PIN_SPI_DATA            PA5
+//#define PIN_SPI_SLAVE_SELECT    PA4
+//#define PIN_SPI_CLOCK           PA7
+
